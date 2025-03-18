63a1c415c29bf89016c0befdd044c73b09cdc9f6
Fatima Zahra <zayani.fatima.z@gmail.com>
2025-03-18 14:10:06 +0100

    ex00

---
+++ ex00/main.cpp
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: fzayani <fzayani@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/03/18 14:00:04 by fzayani           #+#    #+#             */
+/*   Updated: 2025/03/18 14:01:04 by fzayani          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Whatever.hpp"
+#include <iostream>
+#include <string>
+
+int main( void ) {
+	int a = 2;
+	int b = 3;
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
